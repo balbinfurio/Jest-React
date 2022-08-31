e1a739687e5c2cab2511f072f1180b33b0cf654f
balbinfurio <xxbalbin@email.com>
2022-08-31 18:36:59 -0500

    first

---
+++ jsbase/src/__test__/global.test.js
@@ -0,0 +1,28 @@
+const text = 'Hello World';
+const fruits = ['banana', 'apple', 'kiwi'];
+
+test('must contain a string', () => {
+  expect(text).toMatch(/World/);
+});
+
+test('do we have an apple', () => {
+  expect(fruits).toContain('apple');
+});
+
+test('number greater than', () => {
+  expect(10).toBeGreaterThan(9);
+});
+
+test('true', () => {
+  expect(true).toBeTruthy();
+});
+
+const reverseString = (str, callback) => {
+  callback(str.split('').reverse().join(''));
+};
+
+test('test a callback', () => {
+  reverseString('Hello', (str) => {
+    expect(str).toBe('olleH');
+  });
+});
